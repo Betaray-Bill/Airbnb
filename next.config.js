4427fc5e3491c78b5a8550b0f52610b50276fcbb
Betaray-Bill <suryaorion12@gmail.com>
2021-10-12 18:59:33 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -6,3 +6,7 @@ module.exports = {
         mapbox_key: "pk.eyJ1IjoicnN1cnlhIiwiYSI6ImNrczFtODA2YTA0dTEyb3BmNXA1N3ZvZnoifQ.QG6EM3E1CB2gQ_NnGfcEIw"
     }
 }
+
+module.exports = {
+    target: "serverless"
+  }
