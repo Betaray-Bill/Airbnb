cc7d1f01a7c60b72aa06b096d26560619a549ba9
Betaray-Bill <suryaorion12@gmail.com>
2021-10-12 15:30:50 +0530

    out

--- Layout/Hero.js
+++ Layout/Hero.js
@@ -2,7 +2,7 @@ import Header from "../Components/Header";
 
 function Hero() {
     return (
-        <div className="relative h-[600px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]"
+        <div className="relative h-[600px] sm:h-[450px] lg:h-[500px] xl:h-[600px] 2xl:h-[750px]"
             style={{
                 backgroundImage:"url('https://a0.muscache.com/im/pictures/e4a2a61c-589f-4e49-b3b8-968a6bc23389.jpg?im_w=2560')",
                 backgroundRepeat:"no-repeat",
@@ -15,15 +15,15 @@ function Hero() {
             >
             <Header />
 
-            <div className="z-30">
-                <div className="absolute items-center left-12 md:top-24 md:left-32 pt-2">
+            <div className="z-30 -top-4 ">
+                <div className="absolute items-center -top-4 left-12 md:top-24 md:left-32 pt-2">
                     <h2 className="text-white w-72 text-4xl">
                         Olympian & 
                         Paralympian 
                         Online 
                         Experiences
                     </h2>
-                    <button className="text-md text-gray-900 font-bold bg-white px-4 py-4 rounded-lg mt-8 hover:bg-gray-900 hover:scale-105 hover:text-white transition tranform duration-200 ease-out">Explore now</button>
+                    <button className="text-md text-gray-900 font-bold py-3 px-4 text-sm mt-4 bg-white md:px-4 md:py-4 rounded-lg md:mt-8 hover:bg-gray-900 hover:scale-105 hover:text-white transition tranform duration-200 ease-out">Explore now</button>
                 </div>
             </div>
 
